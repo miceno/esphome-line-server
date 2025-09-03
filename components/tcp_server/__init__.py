44fde78855b79e64740e44127dae09bf99025f12
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-03 09:53:46 +0200

    Revert "Allow configuration as an Entity."
    This reverts commit 82bb73764133f5f30b9eca32e6733fd6c9d5570b.

--- components/tcp_server/__init__.py
+++ components/tcp_server/__init__.py
@@ -10,7 +10,6 @@ from esphome.const import (
     CONF_PORT,
 )
 from esphome.core import CORE
-from esphome.core.entity_helpers import entity_duplicate_validator, setup_entity
 
 CONF_TCP_BUFFER_SIZE = "tcp_buffer_size"
 CONF_TCP_TERMINATOR = "tcp_terminator"
@@ -61,9 +60,6 @@ async def setup_tcp_server(var, config):
     # Only add the ID if it is not present in config
     if not CORE.has_id(config[CONF_ID]):
         var = cg.Pvariable(config[CONF_ID], var)
-
-    await setup_entity(var, config, "cover")
-
     cg.add(var.set_port(config[CONF_PORT]))
     cg.add(var.set_tcp_buffer_size(config[CONF_TCP_BUFFER_SIZE]))
     cg.add(var.set_tcp_terminator(config[CONF_TCP_TERMINATOR]))
