38fff544c51cc9033cec3e5ec7d208963144cd2f
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-03 09:27:31 +0200

    Try to generate the id

--- components/rolloffino/__init__.py
+++ components/rolloffino/__init__.py
@@ -51,7 +51,7 @@ def validate_terminator(value):
 REQUIRES_ESPHOME_VERSION = cv.require_esphome_version(2022, 3, 0)
 # Validate only the rolloffino-specific schema additions
 ROLLOFFINO_SCHEMA = cv.Schema({
-    cv.Required(CONF_ID): cv.declare_id(RolloffinoComponent),
+    cv.GenerateID(CONF_ID): cv.declare_id(RolloffinoComponent),
 
     cv.Required(CONF_OPENED_SENSOR): cv.use_id(binary_sensor.BinarySensor),
     cv.Required(CONF_CLOSED_SENSOR): cv.use_id(binary_sensor.BinarySensor),
