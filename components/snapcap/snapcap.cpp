199d7fd7338dfb06a59012bcacb6663d26c12cab
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-03 21:41:14 +0200

    Use direct char access for parsing the command.

--- components/snapcap/snapcap.cpp
+++ components/snapcap/snapcap.cpp
@@ -17,65 +17,71 @@ void SnapCapComponent::process_command(const std::string &command) {
     // Use a static buffer for all responses to minimize stack usage
     static char buf[32];
     std::string response;
-    if (command.rfind(">O000", 0) == 0) {
+    const char *command_cstr = command.c_str();
+    if (command.empty() || command[0] != '>') {
+				response = "*ERR\n";
+				this->send_response(response);
+				return;
+		}
+    if (command_str[1] == 'O') {
         // Open (small steps)
         response = "*O000\n";
         cover_status_ = 1; // open
         servo_status_ = 1; // running
-    } else if (command.rfind(">o000", 0) == 0) {
+    } else if (command_str[1] == 'o') {
         // Force open (one step)
         response = "*o000\n";
         cover_status_ = 1;
         servo_status_ = 1;
-    } else if (command.rfind(">C000", 0) == 0) {
+    } else if (command_str[1] == 'C') {
         // Close (small steps)
         response = "*C000\n";
         cover_status_ = 2; // closed
         servo_status_ = 1;
-    } else if (command.rfind(">c000", 0) == 0) {
+    } else if (command_str[1] == 'c') {
         // Force close (one step)
         response = "*c000\n";
         cover_status_ = 2;
         servo_status_ = 1;
-    } else if (command.rfind(">P000", 0) == 0) {
+    } else if (command_str[1] == 'P') {
         // Ping response and state in one buffer
         snprintf(buf, sizeof(buf), "*P%02d000\n*S%d%d%d\n", device_id_, servo_status_, light_status_, cover_status_);
         response = buf;
-    } else if (command.rfind(">B", 0) == 0 && command.size() >= 5) {
+    } else if (command_str[1] == 'B') && command.size() >= 5) {
         // Set brightness
         int val = std::stoi(command.substr(2, 3));
         brightness_ = val;
         snprintf(buf, sizeof(buf), "*B%d\n", brightness_);
         response = buf;
-    } else if (command.rfind(">J000", 0) == 0) {
+    } else if (command_str[1] == 'J') {
         // Get brightness
         snprintf(buf, sizeof(buf), "*B%d\n", brightness_);
         response = buf;
-    } else if (command.rfind(">L000", 0) == 0) {
+    } else if (command_str[1] == 'L') {
         // Light on
         light_on_ = true;
         light_status_ = 1;
         response = "*L000\n";
-    } else if (command.rfind(">D000", 0) == 0) {
+    } else if (command_str[1] == 'D') {
         // Light off
         light_on_ = false;
         light_status_ = 0;
         response = "*D000\n";
-    } else if (command.rfind(">V000", 0) == 0) {
+    } else if (command_str[1] == 'V') {
         // Firmware version
         snprintf(buf, sizeof(buf), "*V%s\n", firmware_version_.c_str());
         response = buf;
-    } else if (command.rfind(">M000", 0) == 0) {
+    } else if (command_str[1] == 'M') {
         // Get servo position
         snprintf(buf, sizeof(buf), "*M%d\n", servo_position_);
         response = buf;
-    } else if (command.rfind(">N", 0) == 0 && command.size() >= 5) {
+    } else if (command_str[1] == 'N') && command.size() >= 5) {
         // Move servo position
         int pos = std::stoi(command.substr(2, 3));
         servo_position_ = pos;
         snprintf(buf, sizeof(buf), "*N%d\n", servo_position_);
         response = buf;
-    } else if (command.rfind(">W000", 0) == 0) {
+    } else if (command_str[1] == 'W') {
         // Alternate wifi/serial
         response = "*W000\n";
     } else {
