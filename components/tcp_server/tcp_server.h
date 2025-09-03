ce91a71eefb54cd924f112586cf1ee1bea206fd7
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-04 01:43:32 +0200

    Fix wrong header

--- components/tcp_server/tcp_server.h
+++ components/tcp_server/tcp_server.h
@@ -8,7 +8,7 @@
 #include <functional>
 #include "esphome/core/component.h"
 #include "esphome/components/socket/socket.h"
-#include "esphome/components/network/utils.h"
+#include "esphome/components/network/util.h"
 #include "esphome/components/tcp_server/ring_buffer.h"
 
 using esphome::tcp_server::RingBuffer;
