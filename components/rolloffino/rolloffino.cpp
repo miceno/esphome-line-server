ed789913c2936633a08738f877b59e7ab8c54c5c
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2025-09-08 16:41:39 +0200

    Remove redundant check since in1 and in2 are mandatory params.

--- components/rolloffino/rolloffino.cpp
+++ components/rolloffino/rolloffino.cpp
@@ -109,29 +109,25 @@ void RolloffinoComponent::process_command(const std::string &command){
 void RolloffinoComponent::motor_open_() {
     ESP_LOGI(TAG, "Opening motor");
     // Start non-blocking open sequence using PWM
-    if (this->in1_pin_ != nullptr && this->in2_pin_ != nullptr) {
-        this->motor_direction_ = MOTOR_OPEN;
-        this->motor_active_ = true;
-        this->motor_move_start_time_ = esphome::micros();
+    this->motor_direction_ = MOTOR_OPEN;
+    this->motor_active_ = true;
+    this->motor_move_start_time_ = esphome::micros();
 
-        // analogWrite(this->in1_pin_->get_pin(), map(this->duty_cycle_, 0, 100, 0, 255));  // NOLINT
-        this->in1_pin_->digital_write(true);
-        this->in2_pin_->digital_write(false);
-    }
+    // analogWrite(this->in1_pin_->get_pin(), map(this->duty_cycle_, 0, 100, 0, 255));  // NOLINT
+    this->in1_pin_->digital_write(true);
+    this->in2_pin_->digital_write(false);
 }
 
 void RolloffinoComponent::motor_close_() {
     ESP_LOGI(TAG, "Closing motor");
     // Start non-blocking close sequence using PWM
-    if (this->in1_pin_ != nullptr && this->in2_pin_ != nullptr) {
-        this->motor_direction_ = MOTOR_CLOSE;
-        this->motor_active_ = true;
-        this->motor_move_start_time_ = esphome::micros();
+    this->motor_direction_ = MOTOR_CLOSE;
+    this->motor_active_ = true;
+    this->motor_move_start_time_ = esphome::micros();
 
-        this->in1_pin_->digital_write(false);
-        // analogWrite(this->in2_pin_->get_pin(), map(this->duty_cycle_, 0, 100, 0, 255));  // NOLINT
-        this->in2_pin_->digital_write(true);
-    }
+    this->in1_pin_->digital_write(false);
+    // analogWrite(this->in2_pin_->get_pin(), map(this->duty_cycle_, 0, 100, 0, 255));  // NOLINT
+    this->in2_pin_->digital_write(true);
 }
 
 void RolloffinoComponent::motor_abort_() {
