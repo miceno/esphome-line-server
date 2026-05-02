2dd466a85e8775dd534661ca4c61c56bd9192046
Orestes Sanchez Benavente <orestes@estotienearreglo.es>
2026-05-02 23:59:09 +0200

    Rename method to improve readibility.

--- components/rolloffino_cover/__init__.py
+++ components/rolloffino_cover/__init__.py
@@ -52,7 +52,7 @@ def _limit_output_pin_schema(value):
     return pins.internal_gpio_output_pin_schema(value)
 
 
-LIMIT_SWITCH_PIN_SCHEMA = _limit_output_pin_schema
+LIMIT_OUTPUT_PIN_SCHEMA = _limit_output_pin_schema
 
 
 CONFIG_SCHEMA = cv.All(
@@ -60,8 +60,8 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.Required(CONF_OPENED_SENSOR): cv.use_id(binary_sensor.BinarySensor),
             cv.Required(CONF_CLOSED_SENSOR): cv.use_id(binary_sensor.BinarySensor),
-            cv.Required(CONF_IN1_PIN): LIMIT_SWITCH_PIN_SCHEMA,
-            cv.Required(CONF_IN2_PIN): LIMIT_SWITCH_PIN_SCHEMA,
+            cv.Required(CONF_IN1_PIN): LIMIT_OUTPUT_PIN_SCHEMA,
+            cv.Required(CONF_IN2_PIN): LIMIT_OUTPUT_PIN_SCHEMA,
             cv.Optional(CONF_DUTY_CYCLE, default="100"): cv.int_range(min=0, max=100),
             cv.Optional(CONF_MAX_DURATION, default="30s"): cv.positive_time_period_seconds,
         }
